412eb2c6ffb26a7efed499cfe3591aed7332bf07
Ramulu Suddewar <ramulu_suddewar@apple.com>
2020-10-09 14:32:57 +0530

    Added logic for deployment artifact, and calling the service through api gateway

--- sample-python-service-client/src/main/java/demos/gcp/serviceclients/python/SamplePythonServiceClientApplication.java
+++ sample-python-service-client/src/main/java/demos/gcp/serviceclients/python/SamplePythonServiceClientApplication.java
@@ -1,8 +1,9 @@
-package demos.gcp.services.python;
+package demos.gcp.serviceclients.python;
 
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +14,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import com.google.gson.Gson;
 
 @SpringBootApplication
 public class SamplePythonServiceClientApplication {
@@ -30,15 +29,34 @@ class PythonServiceClient {
 
 	@Autowired
 	private RestTemplate restTemplate;
+	
+	@Value("${bypass.api.gateway}")
+	private String bypassAPIGateWay;
+
+	private static String baseServiceURL = "https://sample-python-service-dot-cloudfunctionsdemo-291704.uc.r.appspot.com";
+
+	private static String baseApiURL = "https://sample-python-api-gateway-dkpu0pz4.uc.gateway.dev";
+
+	private static String apiKey = "AIzaSyDSzv7mLkg3a1CwMMrglZ3J0t44GoKtj4g";
+
+	private String serviceUrl;
+
+	private String serviceURI = "/sample-response";
 
 	@GetMapping("/sample-response")
 	public String fetchResponse() {
+
+		if (bypassAPIGateWay.isEmpty() || !Boolean.getBoolean(bypassAPIGateWay)) {
+			serviceUrl = baseApiURL + serviceURI + "?key=" + apiKey;
+		} else {
+			serviceUrl = baseServiceURL + serviceURI;
+
+		}
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 		HttpEntity<String> entity = new HttpEntity<String>(headers);
-		return restTemplate.exchange(
-				"https://sample-python-service-dot-cloudfunctionsdemo-291704.uc.r.appspot.com/sample-response",
-				HttpMethod.GET, entity, String.class).getBody();
+		return restTemplate.exchange(serviceUrl, HttpMethod.GET, entity, String.class).getBody();
 	}
 
 	@Bean
